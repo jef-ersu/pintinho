277167d46bfc9d871b2caecae81830887b8740fb
jef-ersu <santanajefferson594@gmail.com>
2024-11-11 00:59:03 -0300

    Gerador de senhas fortes

--- gris.py
+++ gris.py
@@ -1,5 +1,12 @@
-import os 
-print("My first Git project!!")
-print("hello!")
+import string
+import random
 
-# in comming
+
+def gerar_senha(tamanho = 12):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
+    return senha
+
+tamanho = int(input("Digite o tamanho da senha que deseja (ex.: 12): "))
+senha_forte = gerar_senha(tamanho)
+print("Senha Gerada:", senha_forte)
